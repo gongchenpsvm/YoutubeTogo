f7236182f74070f084d71bbc034f201541287ce4
gongchen <gongchen@usc.edu>
2017-01-05 16:36:00 -0800

    Implement Youtube player's initialization and onInitializationFailure method

--- app/src/main/java/chen/gong/youtubetogo/YoutubeActivity.java
+++ app/src/main/java/chen/gong/youtubetogo/YoutubeActivity.java
@@ -1,17 +1,39 @@
 package chen.gong.youtubetogo;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
 
 public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
+    static final String GOOGLE_API_KEY = "AIzaSyBVFNqZ1g0yzHktuBDptRLst9mx2WR8jzA";
+    static final String YOUTUBE_VIDEO_ID = "OQ5jsbhAv_M";
+    static final String YOUTBE_PLAYLIST = "PLUl4u3cNGP61Oq3tWYp6V_F-5jb5L2iHb";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+//        setContentView(R.layout.activity_youtube);
+        ConstraintLayout layout = (ConstraintLayout)getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(layout);
+
+//        Button button1 = new Button(this);
+//        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300,80));
+//        button1.setText("Button added");
+//        layout.addView(button1);
+        YouTubePlayerView playerView = new YouTubePlayerView(this);
+        playerView.setLayoutParams(new ConstraintLayout.LayoutParams
+                (ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(playerView);
+        playerView.initialize(GOOGLE_API_KEY, this);//If initialized successfully, call the first
+
+
     }
 
     @Override
@@ -21,6 +43,14 @@ public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlaye
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        final int REQUEST_CODE = 1;
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
+        } else {
+            String errorMessage = String.format("Error in initializing the Youtube Player (%1$s)",
+                    youTubeInitializationResult.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
 
     }
 }
